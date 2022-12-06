81c85af8310ed89d1550056d0a41b7c791372bb4
sriram-500apps <sriram.pothuraju@500apps.com>
2022-12-06 23:19:49 +0530

    Vuejs and webextension-toolbox setup

---
+++ webextension-toolbox.config.js
@@ -0,0 +1,33 @@
+const webpack = require("webpack");
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    webpack: (config, {
+        src,
+        target,
+        dev,
+        vendor
+    }) => {
+        config.resolve.extensions.push(".vue");
+        config.module.rules.push(
+            {
+                test: /\.vue$/,
+                loader: "vue-loader"
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    "css-loader"
+                ]
+            }
+        );
+        config.plugins.push(new VueLoaderPlugin());
+        config.plugins.push(new MiniCssExtractPlugin());
+
+        return config
+    }
+}
