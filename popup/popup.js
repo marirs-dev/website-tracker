16d313c56d7e0487c417e5d65e6aa3d42a3c3a42
sriram-500apps <sriram.pothuraju@500apps.com>
2022-12-06 22:58:37 +0530

    popup files added

---
+++ popup/popup.js
@@ -0,0 +1,7 @@
+
+document.getElementById("start").addEventListener("click", (event) => {
+    console.log("start event", event)
+});
+document.getElementById("reset").addEventListener("click", (event) => {
+    console.log("reset event", event)
+});
